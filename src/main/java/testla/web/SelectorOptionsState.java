00dc5cd8e2bc5be7658e9b23f8b4a73cb22dcf12
munichbughunter <munichbughunter@gmail.com>
2022-12-06 15:46:22 +0100

    Fix javadoc in SelectorOptionsState

--- src/main/java/testla/web/SelectorOptionsState.java
+++ src/main/java/testla/web/SelectorOptionsState.java
@@ -1,8 +1,24 @@
 package testla.web;
 
+/**
+ * Represents the selector options state.
+ */
 public enum SelectorOptionsState {
+    /**
+     * Element to be present in DOM
+     */
     ATTACHED,
+    /**
+     * Element to not be present in DOM
+     */
     DETACHED,
+    /**
+     * Element to have non-empty bounding box and no visibility:hidden.
+     * That element without any content or with display:none has an empty bounding box and is not considered visible
+     */
     VISIBLE,
+    /**
+     * Element to be either detached from DOM, or have an empty bounding box or
+     */
     HIDDEN
 }
