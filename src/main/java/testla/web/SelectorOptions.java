3e026c217cd1fb32ca6f758267c83e82ccf80d09
munichbughunter <munichbughunter@gmail.com>
2022-12-08 08:25:06 +0100

    Fix javadoc in SelectorOptions

--- src/main/java/testla/web/SelectorOptions.java
+++ src/main/java/testla/web/SelectorOptions.java
@@ -22,26 +22,54 @@ public class SelectorOptions {
         // like Playwright.
     }
 
+    /**
+     * Advanced selector option - hasText
+     *
+     * @param hasText expected text of the locator
+     * @return this instance
+     */
     public SelectorOptions setHasText(String hasText) {
         this.hasText = hasText;
         return this;
     }
 
+    /**
+     * Advanced selector option - timeout
+     *
+     * @param timeout timeout to interact with locator
+     * @return this instance
+     */
     public SelectorOptions setTimeout(Double timeout) {
         this.timeout = timeout;
         return this;
     }
 
+    /**
+     * Advanced selector option - subSelector
+     *
+     * @param subSelector the selector under a selector
+     * @return this instance
+     */
     public SelectorOptions setSubSelector(SubSelector subSelector) {
         this.subSelector = subSelector;
         return this;
     }
 
+    /**
+     * Advanced selector option - state
+     * @param state the state of the selector
+     * @return this instance
+     */
     public SelectorOptions setSelectorOptionsState(SelectorOptionsState state) {
         this.state = state;
         return this;
     }
 
+    /**
+     * Advanced selector option - replacements
+     * @param replacements the replacements on the selector
+     * @return this instance
+     */
     public SelectorOptions setReplacements(Object... replacements) {
         this.replacements = replacements;
         return this;
