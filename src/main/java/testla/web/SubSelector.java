25c4cabac20def2d9fe1984de7a6e3ea7dc122a8
munichbughunter <munichbughunter@gmail.com>
2022-12-08 08:41:24 +0100

    Fix javadoc in SubSelector

--- src/main/java/testla/web/SubSelector.java
+++ src/main/java/testla/web/SubSelector.java
@@ -3,15 +3,27 @@ package testla.web;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Class that represents a subselector for another selector.
+ * Class that represents a SubSelector for another selector.
  */
 public class SubSelector {
+
+    /**
+     * The selector
+     */
     public String selector;
 
+    /**
+     * The selector options
+     */
     @Nullable
     public SelectorOptions selectorOptions;
 
-
+    /**
+     * Constructor for the SubSelector
+     *
+     * @param selector - the selector for the SubSelector
+     * @param selectorOptions - the selector options for the SubSelector
+     */
     public SubSelector(String selector, @Nullable SelectorOptions selectorOptions) {
         this.selector = selector;
         this.selectorOptions = selectorOptions;
