4a555f632428c779aed7884b9a8951c37cd285b7
munichbughunter <munichbughunter@gmail.com>
2022-12-08 09:36:42 +0100

    Fix javadoc in web/Modes

--- src/main/java/testla/web/Modes.java
+++ src/main/java/testla/web/Modes.java
@@ -1,8 +1,23 @@
 package testla.web;
 
+/**
+ * Represents the web modes.
+ */
 public enum Modes {
+    /**
+     * Element enabled
+     */
     ENABLED,
+    /**
+     * Element disabled
+     */
     DISABLED,
+    /**
+     * Element visible
+     */
     VISIBLE,
+    /**
+     * Element hidden
+     */
     HIDDEN
 }
